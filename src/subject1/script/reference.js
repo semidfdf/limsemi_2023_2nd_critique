4d2cd476286bd0f9adadeccafbc0ba1959fa8338
semidfdf <144070590+semidfdf@users.noreply.github.com>
2023-10-03 10:31:21 +0900

    임샘이_2차_졸업심사

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,7 +1,14 @@
 const reference = [
   {
-    author: "",
-    title: "",
+    author: "이은서",
+    title: "202021409",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "임샘이",
+    title: "202020329",
     year: "",
     sourceTitle: "",
     link: "",
